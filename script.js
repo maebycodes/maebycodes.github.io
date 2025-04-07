dbcf59900087877b88891ef7796ff24c767fb2a1
Maeby <maeby.ever@gmail.com>
2025-04-07 12:01:46 +0200

    Use script to dynamically set email for ALL instances on index as well as dancehole

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
     const encodedEmail = 'bWFlYnlAbWFlYnkubWU=';
     const email = atob(encodedEmail);
-    const link = document.getElementById('email-link');
-    link.href = 'mailto:' + email;
-}); 
+
+    // modify all links with the email-link class
+    const links = document.getElementsByClassName('email-link');
+    for (let link of links) {
+        link.href = 'mailto:' + email;
+    }
+
+    // modify all text elements with the email-text class
+    const texts = document.getElementsByClassName('email-text');
+    for (let text of texts) {
+        text.textContent = email;
+    }
+});
